bd1a13174faca2eb745f208dd41eebdb30f7c520
Greg Kersik <gregkersik@gmail.com>
2019-07-19 10:32:53 +0300

    endpoints for users and tasks added

--- src/index.js
+++ src/index.js
@@ -12,17 +12,64 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
         res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
+    }).catch((err) => {
+        res.status(400).send(err)
     })
+})
+
+//get all users
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch(() => {
+        res.status(500).send()
+    });
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    console.log(_id);
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((err) => {
+        res.status(500).send(err)
+    });
+    
 })
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
         res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
+    }).catch((err) => {
+        res.status(400).send(err)
+    });
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((err) => {
+        res.status(500).send()
+    });
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        
+        res.send(user)
+    }).catch((err) => {
+        res.status(500).send(err)
     });
 })
 
