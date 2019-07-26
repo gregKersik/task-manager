3bdf9cd9f3fd61c3c50b7b645856ea01d4086f0a
Greg Kersik <gregkersik@gmail.com>
2019-07-26 10:23:48 +0300

    passwords hashing

--- src/index.js
+++ src/index.js
@@ -1,189 +1,30 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e){
-        res.status(400).send(e)
-    }
-    // user.save().then(() => {
-    //     res.status(201).send(user)
-    // }).catch((err) => {
-    //     res.status(400).send(err)
-    // })
-})
-
-//get all users
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-                }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send(err)
-    }
-
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(user)
-    // }).catch((err) => {
-    //     res.status(500).send(err)
-    // });
-    
-})
-
-app.patch('/users/:id', async(req, res) => {
-    const update = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = update.every(update => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'Inavalid updates!'})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body,
-                                                     {new: true, runValidators: true})  
-        
-        if (!user) {
-            return res.status(401).send()
-        }                                
-        
-        res.send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (error) {
-        res.status(400).send()
-    }
-    // task.save().then(() => {
-    //     res.status(201).send(task)
-    // }).catch((err) => {
-    //     res.status(400).send(err)
-    // });
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (error) {
-        res.status(500).send()
-    }
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks)
-    // }).catch((err) => {
-    //     res.status(500).send()
-    // });
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await Task.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send(err)
-    }
-    // Task.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-        
-    //     res.send(user)
-    // }).catch((err) => {
-    //     res.status(500).send(err)
-    // });
-})
-
-
-
-app.patch('/tasks/:id', async (req, res) => {
-    const update = Object.keys(req.body)
-    const allowedUpdates = ['completed', 'description']
-    const isValidOperation = update.every(update => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: 'Inavalid updates!'})
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body,
-                                                 {new: true, runValidators: true})  
-
-        if (!task) {
-            return res.status(401).send()
-        }
-
-        res.send(task)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port', port);
 })
+
+// const bcrypt = require('bcrypt')
+
+// const myFunction = async () => {
+//     const password = 'Res123565!'
+//     const heshedPssword = await bcrypt.hash(password, 8)
+
+//     console.log(password);
+//     console.log(heshedPssword);
+
+//     const isMatch = await bcrypt.compare('Res123565!', heshedPssword)
+//     console.log(isMatch);
+// }
+
+// myFunction()
